4b14d2cd1c53f5ee17d704ea6dbad9833d9c6fa2
Anton Gubernatorov <gubernatorov.tosha@gmail.com>
2024-10-19 12:13:59 +0300

    "feat: добавлена валидация форм"

--- src/main/java/ru/gubern/tacocloud/controller/DesignTacoController.java
+++ src/main/java/ru/gubern/tacocloud/controller/DesignTacoController.java
@@ -1,8 +1,10 @@
 package ru.gubern.tacocloud.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import ru.gubern.tacocloud.tacos.Ingredient;
 import ru.gubern.tacocloud.tacos.Taco;
@@ -58,8 +60,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco,
+    public String processTaco(@Valid Taco taco, Errors errors,
                               @ModelAttribute TacoOrder tacoOrder){
+        if (errors.hasErrors()){
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
--- src/test/java/ru/gubern/tacocloud/controller/HomeControllerTest.java
+++ src/test/java/ru/gubern/tacocloud/controller/HomeControllerTest.java
@@ -5,13 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.gubern.tacocloud.config.WebConfig;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 class HomeControllerTest {
 
     @Autowired
